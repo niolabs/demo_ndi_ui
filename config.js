9adc10a637e05b434dd233d5cce3d9864913cdd0
deliciousmonster <jaxon@deliciousmonster.com>
2018-04-25 15:15:36 -0600

    token swap

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export default {
       PK_HOST: 'pk.demo.niolabs.com',
       PK_PORT: 443,
       PK_SECURE: true,
-      PK_JWT: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuZGkiLCJpc3MiOiJwdWJrZWVwZXIiLCJhdWQiOiJwdWJrZWVwZXIiLCJwZXJtIjpbImFsbCJdfQ.gSeJySkEB3nGlQ4FH_im9D3sk6TlSi_Ax5J1XDOSRX4',
+      PK_JWT: 'supersecretdemotokenzforthetokendemoz',
       WS_HOST: 'ws.demo.niolabs.com',
       WS_PORT: 443,
       WS_SECURE: true,
