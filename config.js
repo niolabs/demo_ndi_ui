4bcc927df519d66c09eb7306d8b432c2648a2d4c
deliciousmonster <jaxon@deliciousmonster.com>
2018-09-25 15:05:58 -0600

    use new pk server

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ export default {
   pubkeeper: {
     staticPubkeeper: true,
     pkConfig: {
-      PK_HOST: 'pk.demo.niolabs.com',
+      PK_HOST: 'pks.demo.niolabs.com',
       PK_PORT: 443,
       PK_SECURE: true,
-      PK_JWT: 'supersecretdemotokenzforthetokendemoz',
-      WS_HOST: 'ws.demo.niolabs.com',
+      PK_JWT: '7BB1F97195DE7CA6D0CF0D3AE821BB6916C7CD0053E8E4610EECD34AEEFDB4F0',
+      WS_HOST: 'wss.demo.niolabs.com',
       WS_PORT: 443,
       WS_SECURE: true,
     },
